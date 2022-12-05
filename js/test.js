70a95e2b9f8aa253fc90f842d84c6881ee4363da
jin <schoolyoung2m@naver.com>
2022-12-05 18:15:47 +0900

    2022.12.05 / 상하 롤링 구현중2

---
+++ js/test.js
@@ -0,0 +1,9 @@
+//left박스 태그
+//left 박스 안 이미지들 태그
+
+//트렌스폼에 대입할 변수하나 생성
+//let trans_num
+//자동반복함수로 trans_num -= 1 해줌
+//태그에 대입 left_img.style.transform = `translateY(${trans_num}%)`
+//조건문으로 -100%쯤 되었을 때
+//첫번째 요소 맨 마지막에 넣음 left.append(img첫번째 요소)
